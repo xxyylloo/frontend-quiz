04ced7b0921347d61cdd99731ff90a2b479bcc6a
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-10-11 20:41:55 +0200

    added daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
 <h1 class="text-5xl text-red-500">Homepage with Tailwind</h1>
+
+<!-- Open the modal using ID.showModal() method -->
+<button class="btn" onclick="my_modal_1.showModal()">...and DaisyUI (click)</button>
+<dialog id="my_modal_1" class="modal">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Hello from Daisy 🌻!</h3>
+		<p class="py-4">Press ESC key or click the button below to close</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
