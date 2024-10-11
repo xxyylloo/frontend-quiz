73e92ab4ba5352a76e9fc58d5b24c8ff3c7a5599
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-10-11 22:31:53 +0200

    added iconify, made dark/light theme switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.css';
+	import 'iconify-icon';
+	let theme = 'light';
+	function switchTheme() {
+		theme = theme === 'light' ? 'dark' : 'light';
+	}
 </script>
 
-<slot />
+<div data-theme={theme} class="min-h-screen">
+	<div class="absolute top-4 right-4">
+		<iconify-icon icon="lucide-sun" class="text-2xl"></iconify-icon>
+		<input type="checkbox" class="toggle" on:click={switchTheme} />
+		<iconify-icon icon="lucide-moon" class="text-2xl"></iconify-icon>
+	</div>
+	<slot />
+</div>
