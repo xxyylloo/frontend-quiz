6597d5e339fb8dbb3d187a82f74e6dd811170ab4
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-10-13 13:27:07 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,9 +9,9 @@
 
 <div data-theme={theme} class="min-h-screen">
 	<div class="absolute top-4 right-4">
-		<iconify-icon icon="lucide-sun" class="text-2xl"></iconify-icon>
-		<input type="checkbox" class="toggle" on:click={switchTheme} />
 		<iconify-icon icon="lucide-moon" class="text-2xl"></iconify-icon>
+		<input type="checkbox" class="toggle" on:click={switchTheme} />
+		<iconify-icon icon="lucide-sun" class="text-2xl"></iconify-icon>
 	</div>
 	<slot />
 </div>
